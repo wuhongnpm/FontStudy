4c0543d31f8bef95497fa2bfc5c38629445394a4
SoapEye <lious.q.k@gmail.com>
2021-05-26 17:13:56 +0800

    1.XINJ

---
+++ websoap/src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+          <p>That I did always love</p>
+         <p>I bring thee Proof</p>
+         <p>That till I loved</p>
+         <p>I never lived—Enough</p>
+         <p>That I shall love alway</p>
+         <p>I argue thee</p>
+         <p>That love is life</p>
+         <p>And life hath Immortality</p>
+         <p>This—dost thou doubt—Sweet</p>
+         <p>Then have I</p>
+         <p>Nothing to show</p>
+         <p>But Calvary</p>
+    
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
