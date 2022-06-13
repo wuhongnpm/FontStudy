4168a0ff8a4616dd40c4d5f6672c2c8415ef6ed8
SoapEye <lious.q.k@gmail.com>
2022-06-13 14:22:46 +0800

    2

--- websoap/src/components/HelloWorld.vue
+++ websoap/src/components/HelloWorld.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-          <p>That I did always love</p>
-         <p>I bring thee Proof</p>
-         <p>That till I loved</p>
-         <p>I never lived—Enough</p>
-         <p>That I shall love alway</p>
-         <p>I argue thee</p>
-         <p>That love is life</p>
-         <p>And life hath Immortality</p>
-         <p>This—dost thou doubt—Sweet</p>
-         <p>Then have I</p>
-         <p>Nothing to show</p>
-         <p>But Calvary</p>
-    
+    <p>That I did always love</p>
+    <p>I bring thee Proof</p>
+    <p>That till I loved</p>
+    <p>I never lived—Enough</p>
+    <p>That I shall love alway</p>
+    <p>I argue thee</p>
+    <p>That love is life</p>
+    <p>And life hath Immortality</p>
+    <p>This—dost thou doubt—Sweet</p>
+    <p>Then have I</p>
+    <p>Nothing to show</p>
+    <p>But Calvary</p>
+    <footer>
+      <p><a href="https://beian.miit.gov.cn/#/Integrated/index"> 鲁ICP备18000993号-2</a></p>
+    </footer>
 
   </div>
 </template>
